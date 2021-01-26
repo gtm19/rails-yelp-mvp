d75fec7c45a41ab8b86ef0da6cd1d14ba5123870
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-01-26 21:04:12 +0000

    added categories method to Restaurant and completed seed file

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,11 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
+  def self.categories
+    %w(chinese italian japanese french belgian)
+  end
+
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true, inclusion: { in: %w(chinese italian japanese french belgian) }
+  validates :category, presence: true, inclusion: { in: self.categories }
 end
