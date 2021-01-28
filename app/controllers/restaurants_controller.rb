9fb44301861812318988069b77e74503db42f993
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-01-28 21:36:49 +0000

    challenge complete

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,26 @@
 class RestaurantsController < ApplicationController
 
   before_action :set_restaurant, only: [:show]
+  before_action :get_categories
+  
   def index
     @restaurants = Restaurant.all
   end
 
   def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
   end
   
   private
@@ -13,5 +28,13 @@ class RestaurantsController < ApplicationController
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
     @reviews = @restaurant.reviews
+  end
+
+  def get_categories
+    @categories = Restaurant.categories
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
